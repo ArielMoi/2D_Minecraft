e3429abafe90fe659000a354761016a53caf1168
Ariel Moise <ariel.m23@gmail.com>
2021-02-25 12:06:17 +0200

    update commit

--- script/script.js
+++ script/script.js
@@ -26,24 +26,62 @@ for (let row = 1; row <= 20; row++) { //starts counting from one for easier numb
 // upper land - grass
 landScapeMaker('grass', 14, 14, 1, 25);
 
-//land
 landScapeMaker('soil', 15, 20, 1, 25);
 
-// tree
 landScapeMaker('wood', 10, 13, 20, 20);
 
-// tree leaved
 landScapeMaker('leaves', 7, 9, 19, 21);
 
-// rock
 landScapeMaker('rock', 12, 13, 15, 15);
 landScapeMaker('rock', 13, 13, 2, 2);
 
-// bush
 landScapeMaker('leaves', 13, 13, 5, 7);
 landScapeMaker('leaves', 12, 12, 6, 6);
 
-// cloud
 landScapeMaker('cloud', 5, 5, 9, 13);
 landScapeMaker('cloud', 4, 4, 10, 13);
 landScapeMaker('cloud', 3, 3, 10, 11);
+
+
+const materialObj = {
+    'axe': ['leaves', 'wood'],
+    'picaxe': ['rock'],
+    'shovel': ['soil', 'grass']
+}
+
+const axe = document.querySelector('.axe');
+const picaxe = document.querySelector('.picaxe');
+const shovel = document.querySelector('.shovel');
+const game = document.querySelector('.game-grid');
+
+const inventory = {};
+
+
+
+function collectMaterial(tool, material, event){
+    if (materialObj[tool].includes(material)){
+        console.log(materialObj[tool])
+        inventory[material] ? inventory[material] += 1 : inventory[material] = 1;
+        event.target.classList.remove(material);
+    }
+}
+
+axe.addEventListener('click', () => {
+    game.addEventListener('click', (event) => {
+        collectMaterial('axe', event.target.classList[1], event);
+    })
+})
+
+picaxe.addEventListener('click', () => {
+    game.addEventListener('click', (event) => {
+        collectMaterial('picaxe', event.target.classList[1], event);
+    })
+})
+
+shovel.addEventListener('click', () => {
+    game.addEventListener('click', (event) => {
+        collectMaterial('shovel', event.target.classList[1], event);
+    })
+})
+
+// inventory
